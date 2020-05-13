b6b0a1d7aa792cac11acc3a96d700c49af8df70c
Izabela Bednarczyk <izabela.bednarczyk93@gmail.com>
2020-05-13 21:49:04 +0200

    Set size and fonts of components, replacing JComboBox with JTextField

--- src/main/java/introduction/IntroductionForm.java
+++ src/main/java/introduction/IntroductionForm.java
@@ -19,20 +19,19 @@ public class IntroductionForm implements ActionListener {
     private JLabel professionLabel = new JLabel("Profession");
     private JLabel loveToLabel = new JLabel("Love to");
     private JLabel wantToLabel = new JLabel("Want to");
-    private String[] tech = {"Java", "Spring", "Hibernate", "Git", "REST", "SQL"};
-    private String[] skills = { "Creativity", "Communication", "Problem solving", "Team player"};
-    private JComboBox<String> techBox = new JComboBox<>(tech);
-    private JComboBox<String> skillsBox = new JComboBox<>(skills);
+    private JTextField techField = new JTextField();
+    private JTextField skillField = new JTextField();
     private JLabel techLabel = new JLabel("Languages&Technologies");
     private JLabel skillsLabel  = new JLabel("Skills");
 
-    private JButton seeButton = new JButton("Let's see!");
+
     private JButton helloButton = new JButton("Say hello!");
 
 
     public IntroductionForm() {
         createForm();
         setSize();
+        setFont();
         add();
 
     }
@@ -51,8 +50,30 @@ public class IntroductionForm implements ActionListener {
     private void setSize(){
         firstNameLabel.setBounds(20,20, 200, 40);
         lastNameLabel.setBounds(20, 70, 200, 40);
-        firstNameField.setBounds(220, 20, 240, 40);
-        lastNameField.setBounds(220, 70, 240, 40);
+        firstNameField.setBounds(240, 20, 220, 40);
+        lastNameField.setBounds(240, 70, 220, 40);
+        professionLabel.setBounds(20, 120, 200, 40);
+        professionField.setBounds(240, 120, 220, 40);
+        loveToLabel.setBounds(20, 170, 200, 40);
+        loveToField.setBounds(240, 170, 220, 40);
+        wantToLabel.setBounds(20, 220, 200, 40);
+        wantToField.setBounds(240, 220, 220, 40);
+        techLabel.setBounds(20, 270, 200, 40);
+        skillsLabel.setBounds(20, 320, 200, 40);
+        techField.setBounds(240, 270, 220, 40);
+        skillField.setBounds(240, 320, 220, 40);
+        helloButton.setBounds(180, 400, 120, 50);
+    }
+
+    private void setFont(){
+        firstNameLabel.setFont(new Font("Courier New", Font.BOLD, 14));
+        lastNameLabel.setFont(new Font("Courier New", Font.BOLD, 14));
+        professionLabel.setFont(new Font("Courier New", Font.BOLD, 14));
+        loveToLabel.setFont(new Font("Courier New", Font.BOLD, 14));
+        wantToLabel.setFont(new Font("Courier New", Font.BOLD, 14));
+        techLabel.setFont(new Font("Courier New", Font.BOLD, 14));
+        skillsLabel.setFont(new Font("Courier New", Font.BOLD, 14));
+
     }
 
     private void add(){
@@ -61,8 +82,8 @@ public class IntroductionForm implements ActionListener {
         jFrame.add(professionField);
         jFrame.add(loveToField);
         jFrame.add(wantToField);
-        jFrame.add(techBox);
-        jFrame.add(skillsBox);
+        jFrame.add(techField);
+        jFrame.add(skillField);
         jFrame.add(firstNameLabel);
         jFrame.add(lastNameLabel);
         jFrame.add(professionLabel);
@@ -71,7 +92,6 @@ public class IntroductionForm implements ActionListener {
         jFrame.add(techLabel);
         jFrame.add(skillsLabel);
         jFrame.add(helloButton);
-        jFrame.add(seeButton);
 
     }
 
